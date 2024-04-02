5952a887f8b71fc5e858870b31b5c82a52e9e372
Alex Pishvanov <citikot@dr.com>
2024-04-02 21:09:03 +0300

    initial commit

---
+++ src/test/java/online/agatstudio/topic11/Topic11ApplicationTests.java
@@ -0,0 +1,13 @@
+package online.agatstudio.topic11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Topic11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
